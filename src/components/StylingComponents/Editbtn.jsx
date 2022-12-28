84fc16ab6ea786d18ce8c17cbaf3e6080d7f4cfd
vab096 <vabmanral@gmail.com>
2022-12-28 23:49:10 +0530

    Notification

--- src/components/StylingComponents/Editbtn.jsx
+++ src/components/StylingComponents/Editbtn.jsx
@@ -4,7 +4,11 @@ import Button from '@mui/material/Button';
 export default function BasicButtons(p) {
   return (
     <Stack spacing={0} direction="row">
-      <Button style={{height:p.height,width:p.width,backgroundColor:p.backgroundColor,borderRadius:p.borderRadius,margin:p.margin,marginTop:p.marginTop,marginBottom:p.marginBottom}} variant={p.variant}> {p.icon}  {p.label}</Button>
+      <Button style={{display:"flex",border:p.border,marginLeft:p.marginLeft,color:p.color,height:p.height,width:p.width,backgroundColor:p.backgroundColor,borderRadius:p.borderRadius,margin:p.margin,marginTop:p.marginTop,marginBottom:p.marginBottom,boxSizing:"border-box",paddingTop:"7px",alignItems:"center",justifyContent:"center"}} size={p.size} variant={p.variant}> 
+      <div style={{marginTop:"4px"}}> {p.icon} </div>
+      <div> {p.label}</div>
+  
+      </Button>
     </Stack>
   );
 }
