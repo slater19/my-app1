8405b33f2712aff9590a052167bd86a63d433003
vab096 <vabmanral@gmail.com>
2022-12-30 14:28:39 +0530

    Message

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import './App.css'
 import { Profile } from './components/Profile/Profile'
 import { Search } from './components/Search/Search'
 import { Notification } from './components/Notification/Notification'
+import { Message } from './Messages/Message'
 // import { Profilehistory } from './components/Profilehistory'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       {/* <Profile/> */}
       {/* <Search/> */}
-      <Notification/>
+      {/* <Notification/> */}
+      <Message/>
     </div>
   )
 }
---
+++ src/Messages/Message.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Message.css'
+import Profile from '../Images/Profile.png';
+import {BsArrowLeft} from 'react-icons/bs'
+import {BsSearch} from "react-icons/bs"
+export const Message = () => {
+
+const arr=[{
+    Image:Profile,
+    Name:"Tanmay RatnaParkhi",
+    Profession:"Founder",
+    Message:"Show Message",
+    Time:"19:20 PM"
+},
+{
+    Image:Profile,
+    Name:"Tanmay",
+    Profession:"Founder",
+    Message:"Show Message",
+    Time:"19:20 PM"
+}]
+
+  return (
+    <div className='_MessageMainDiv'>
+        <div className='_MessageDiv1'>
+            <div>
+                <BsArrowLeft fontSize={"30px"} />
+            </div>
+            <div>
+                <span>Message</span>
+            </div>
+            <div>
+                <BsSearch fontSize={"30px"}/>
+            </div>
+        </div>
+        {
+        arr.map((el,i)=>{
+            return(
+            <div className='_MessageDiv2'>
+            <div className='_MessageDiv12'>
+                <div className='_MessageImage'>
+                    <img src={el.Image}/>
+                </div>
+                <div className='_MessageDiv121'>
+                    <div>
+                        {el.Name}
+                    </div>
+                    <div >
+                        <p>{el.Profession}</p>
+                    </div>
+                    <div >
+                        <p>{el.Message}</p>
+                    </div>
+                    <div>
+                        <p>{el.Time}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+            )
+        })
+       
+}
+    </div>
+  )
+}
